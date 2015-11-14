a48bb735d8f4c6e99958f115b2363b79d8e2aa3f
sonir6280 <rohit.soni@steria.co.uk>
2015-11-14 14:09:58 +0000

    Maven Hello World

---
+++ myMavenProject/src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void Login(){
+		System.out.println("Logging into the account");
+	}
+}
